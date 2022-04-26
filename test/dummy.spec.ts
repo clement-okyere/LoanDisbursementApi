59138be164cc0f959c3436fcb3c1c4a3eba36f72
clement <clement@worldreader.org>
2022-04-26 05:29:22 +0000

    installed typescript and webpack

--- test/dummy.spec.ts
+++ test/dummy.spec.ts
@@ -1,6 +1,6 @@
-describe("Test", function() {
-    it("expects expression to be truthy", function() {
-        const condition = 1 === 1;
-        expect(condition).toBeTruthy();
-    })
-})
+describe('Test', function () {
+  it('expects expression to be truthy', function () {
+    const condition = 1 === 1;
+    expect(condition).toBeTruthy();
+  });
+});
