0579234bd7bfe6b4962e41c78445509604083e82
clement <clement@worldreader.org>
2022-04-27 08:45:48 +0000

    add companyId information when creatng a loan

--- src/apps/loanManagement/createLoan.ts
+++ src/apps/loanManagement/createLoan.ts
@@ -3,18 +3,18 @@ import 'source-map-support/register';
 import { createLoan } from '../../businessLogic/loan';
 import { validate } from '../../utils/validation';
 import { loanSchema } from '../../schemas/loan';
-import axios from "axios";
+import axios from 'axios';
 
 const OPENKVK_BASE_URL = process.env.OPENKVK_BASE_URL;
-const OPENKVK_API_KEY   = process.env.OPENKVK_API_KEY;
+const OPENKVK_API_KEY = process.env.OPENKVK_API_KEY;
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   console.log('Processing event: ', event);
 
   // get company Id from query parameter
-  let companyId: string | undefined ;
+  let companyId: string | undefined;
 
-  if(event.queryStringParameters) {
+  if (event.queryStringParameters) {
     companyId = event.queryStringParameters.companyId;
   }
 
@@ -46,45 +46,42 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       }),
     };
 
-    // make a call to the company endpoint 
-    try {
-      const {  data } = await axios.get(`${OPENKVK_BASE_URL}/${companyId}`,
-      {
-        headers: { 
-          'ovio-api-key': OPENKVK_API_KEY
-        }
-      }
-      );
+  // make a call to the company endpoint
+  try {
+    const { data } = await axios.get(`${OPENKVK_BASE_URL}/${companyId}`, {
+      headers: {
+        'ovio-api-key': OPENKVK_API_KEY,
+      },
+    });
 
-      if(!data.actief) {
-        return {
-          statusCode: 400,
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-          },
-          body: JSON.stringify({
-            message: `Company with id ${companyId} is not active`,
-          }),
-        };
-      }
-
-      console.log('company api calll response', data);
-
-      // add company response 
-      newLoan.company = data;
-    } catch (e) {
-      console.log('An error occured while updating the loan status', e);
+    if (!data.actief) {
       return {
         statusCode: 400,
         headers: {
           'Access-Control-Allow-Origin': '*',
         },
         body: JSON.stringify({
-          message: 'An error occured while updating the loan status',
+          message: `Company with id ${companyId} is not active`,
         }),
       };
     }
-    
+
+    console.log('company api calll response', data);
+
+    // add company response
+    newLoan.company = data;
+  } catch (e) {
+    console.log('An error occured while updating the loan status', e);
+    return {
+      statusCode: 400,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({
+        message: 'An error occured while updating the loan status',
+      }),
+    };
+  }
 
   const newItem = await createLoan(newLoan);
 
